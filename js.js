f4afa161e331ccd9cbca934e1da98368e7b07385
Phelyppe Alex <phelyppesilva@gmail.com>
2022-05-11 12:28:21 -0300

    Notificação de ganhou

--- js.js
+++ js.js
@@ -1,25 +1,51 @@
 var jogador = 'X';
+var matriz = new Array(9);
+var casaOcupada;
+var alguemGanhou = false;
 
 function jogada(casa){
+
+    if(matriz[casa-1] == null){
+        casaOcupada = false;
+    }else{
+        casaOcupada = true;
+        alert('Casa ocupada!!');
+        return;
+    }
+
     switch(casa){
-        case 1: if(jogador == 'X'){document.getElementById('1X').style.display = 'block';}else{document.getElementById('1O').style.display = 'block';}
+        case 1: if(jogador == 'X'){document.getElementById('1X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('1O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 2: if(jogador == 'X'){document.getElementById('2X').style.display = 'block';}else{document.getElementById('2O').style.display = 'block';}
+        case 2: if(jogador == 'X'){document.getElementById('2X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('2O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 3: if(jogador == 'X'){document.getElementById('3X').style.display = 'block';}else{document.getElementById('3O').style.display = 'block';}
+        case 3: if(jogador == 'X'){document.getElementById('3X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('3O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 4: if(jogador == 'X'){document.getElementById('4X').style.display = 'block';}else{document.getElementById('4O').style.display = 'block';}
+        case 4: if(jogador == 'X'){document.getElementById('4X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('4O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 5: if(jogador == 'X'){document.getElementById('5X').style.display = 'block';}else{document.getElementById('5O').style.display = 'block';}
+        case 5: if(jogador == 'X'){document.getElementById('5X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('5O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 6: if(jogador == 'X'){document.getElementById('6X').style.display = 'block';}else{document.getElementById('6O').style.display = 'block';}
+        case 6: if(jogador == 'X'){document.getElementById('6X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('6O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 7: if(jogador == 'X'){document.getElementById('7X').style.display = 'block';}else{document.getElementById('7O').style.display = 'block';}
+        case 7: if(jogador == 'X'){document.getElementById('7X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('7O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 8: if(jogador == 'X'){document.getElementById('8X').style.display = 'block';}else{document.getElementById('8O').style.display = 'block';}
+        case 8: if(jogador == 'X'){document.getElementById('8X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('8O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
-        case 9: if(jogador == 'X'){document.getElementById('9X').style.display = 'block';}else{document.getElementById('9O').style.display = 'block';}
+        case 9: if(jogador == 'X'){document.getElementById('9X').style.display = 'block';matriz[casa-1] = 'X';}else{document.getElementById('9O').style.display = 'block';matriz[casa-1] = 'O';}
         break;
+    }
+
+    
+
+    if((matriz[0] == 'X' && matriz[1] == 'X' && matriz[2] == 'X') || (matriz[3] == 'X' && matriz[4] == 'X' && matriz[5] == 'X')
+    || (matriz[6] == 'X' && matriz[7] == 'X' && matriz[8] == 'X') || (matriz[0] == 'X' && matriz[3] == 'X' && matriz[6] == 'X')
+    || (matriz[1] == 'X' && matriz[4] == 'X' && matriz[7] == 'X') || (matriz[2] == 'X' && matriz[5] == 'X' && matriz[8] == 'X')
+    || (matriz[0] == 'X' && matriz[4] == 'X' && matriz[8] == 'X') || (matriz[2] == 'X' && matriz[4] == 'X' && matriz[6] == 'X')
+    || (matriz[0] == 'O' && matriz[1] == 'O' && matriz[2] == 'O') || (matriz[3] == 'O' && matriz[4] == 'O' && matriz[5] == 'O')
+    || (matriz[6] == 'O' && matriz[7] == 'O' && matriz[8] == 'O') || (matriz[0] == 'O' && matriz[3] == 'O' && matriz[6] == 'O')
+    || (matriz[1] == 'O' && matriz[4] == 'O' && matriz[7] == 'O') || (matriz[2] == 'O' && matriz[5] == 'O' && matriz[8] == 'O')
+    || (matriz[0] == 'O' && matriz[4] == 'O' && matriz[8] == 'O') || (matriz[2] == 'O' && matriz[4] == 'O' && matriz[6] == 'O')){
+        alguemGanhou = true;
+        alert('Parabéns, jogador ' + jogador + ', você ganhou!!');
     }
 
     if(jogador == 'X'){
@@ -39,6 +65,12 @@ function reset(){
     document.getElementById('7X').style.display = 'none'; document.getElementById('7O').style.display = 'none';
     document.getElementById('8X').style.display = 'none'; document.getElementById('8O').style.display = 'none';
     document.getElementById('9X').style.display = 'none'; document.getElementById('9O').style.display = 'none';
+    
+    for (let index = 0; index < matriz.length; index++) {
+        matriz[index] = null;
+    }
+
+    alguemGanhou = false;
 
     jogador = 'X';
 }
